8d172c38e4fe024fab718d3c1522094fdad0b099
chiefmikey <wolfemikl@gmail.com>
2021-11-15 10:56:32 -0700

    Update loop 3

--- loop-3.rb
+++ loop-3.rb
@@ -1,6 +1,6 @@
 live_loop :melody do
-  with_fx :hpf, mix: 1, cutoff: 100, amp: 10 do
-    sample :ambi_piano, finish: 0.74
+  with_fx :hpf, mix: 1, cutoff: 100, amp: 15 do
+    sample :ambi_piano, finish: 0.74, amp: 0.8
     sleep 2
     sample :ambi_piano, pitch: -6, finish: 0.4
     sleep 2
@@ -55,7 +55,7 @@ end
 live_loop :hat do
   with_fx :slicer, mix: 1 do
     with_fx :panslicer, wave: 2, pan_max: 0.5, pulse_width: 0.5, seed: 1 do
-      sample :drum_cymbal_closed, amp: 0.3, rate: 1.01, finish: 0.75, env_curve: 2
+      sample :drum_cymbal_closed, amp: 0.28, rate: 1.01, finish: 0.75, env_curve: 2
       sleep 0.25
     end
   end
@@ -77,7 +77,7 @@ live_loop :creep do
 end
 
 live_loop :scratch do
-  with_fx :slicer, amp: 0.75 do
+  with_fx :slicer, amp: 0.8 do
     with_fx :krush do
       with_fx :hpf, cutoff: 75 do
         sample :vinyl_hiss, start: 0.4, finish: 0.5, rate: 1.02, pan: 0.3
@@ -87,10 +87,10 @@ live_loop :scratch do
   end
 end
 
-live_loop :violin do
+live_loop :bass do
   with_fx :lpf, cutoff: 10 do
     with_fx :wobble, wave: 0, mix: 1 do
-      sample :bass_thick_c, rate: 0.50, finish: 0.40, amp: 3, env_curve: 2
+      sample :bass_thick_c, rate: 0.50, finish: 0.40, amp: 5, env_curve: 2
       sleep 1
     end
   end
